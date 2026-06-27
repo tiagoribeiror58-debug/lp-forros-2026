4079590ccaee1cef13f049d6bbf95fa5ad339e5d
Tiago Ribeiro <tiagoribeiror58@gmail.com>
2026-06-27 16:55:15 -0300

    fix: configuração do Vite multi-page e script module

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,18 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        catalogo: resolve(__dirname, 'catalogo.html'),
-      },
-    },
-  },
+        catalogo: resolve(__dirname, 'catalogo.html')
+      }
+    }
+  }
 });
